763263d9d68cc0eefc65816ddb4ab0ec807160d9
DulminiD <dulminid1107@gmail.com>
2020-07-17 12:10:37 +0530

    Edit product function

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,28 @@ import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
 import {ReactiveFormsModule} from '@angular/forms';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
+import { GetChartComponent } from './get-chart/get-chart.component';
+import { ChartsModule } from 'ng2-charts';
+import { GetDoughnutChartComponent } from './get-chart/get-doughnut-chart/get-doughnut-chart.component';
+import { GetBarChartComponent } from './get-chart/get-bar-chart/get-bar-chart.component';
+import { EditProductComponent } from './edit-product/edit-product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    routingComponents
+    routingComponents,
+    GetChartComponent,
+    GetDoughnutChartComponent,
+    GetBarChartComponent,
+    EditProductComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChartsModule
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
